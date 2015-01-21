0d99b85892a1bd84ec98a74789c261e2c53d2d52
Roberto Simoni <me@sixro.net>
2015-01-21 14:22:57 +0100

    Reformatted class due to a strange indentation done by Eclipse

--- main-javafx/src/main/java/trafficlight/main/javafx/TrafficLightUI.java
+++ main-javafx/src/main/java/trafficlight/main/javafx/TrafficLightUI.java
@@ -8,18 +8,18 @@ public class TrafficLightUI extends VBox {
 
 	public TrafficLightUI(int width, int height, TrafficLight trafficLight) {
 		super();
-		
-    	int oneThird = height /3;
+
+		int oneThird = height / 3;
 		LightUI redUI = new LightUI(width, oneThird, Color.RED);
-    	getChildren().add(redUI);
-    	LightUI yellowUI = new LightUI(width, oneThird, Color.ORANGE);
-    	getChildren().add(yellowUI);
-    	LightUI greenUI = new LightUI(width, oneThird, Color.GREEN);
-    	getChildren().add(greenUI);
-    	
-    	trafficLight.addRedLightObserver(redUI);
-    	trafficLight.addGreenLightObserver(greenUI);
-    	trafficLight.addYellowLightObserver(yellowUI);
+		getChildren().add(redUI);
+		LightUI yellowUI = new LightUI(width, oneThird, Color.ORANGE);
+		getChildren().add(yellowUI);
+		LightUI greenUI = new LightUI(width, oneThird, Color.GREEN);
+		getChildren().add(greenUI);
+
+		trafficLight.addRedLightObserver(redUI);
+		trafficLight.addGreenLightObserver(greenUI);
+		trafficLight.addYellowLightObserver(yellowUI);
 	}
 
 }
